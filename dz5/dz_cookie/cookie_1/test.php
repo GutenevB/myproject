02f1f55507836f88bdb00b7e3522811da55bd179
GutenevB <marcus071@mail.ru>
2015-10-10 01:56:12 +0300

    ...

--- dz5/dz_cookie/cookie_1/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-if (isset($_COOKIE['country'])) {
-    echo $_COOKIE['country'];
-}
--- dz5/dz_cookie/cookie_2/cookie.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (isset($_COOKIE['age']) && isset($_COOKIE['age1']) && isset($_COOKIE['age2']) && isset($_COOKIE['age3'])) {
-    echo $_COOKIE['age'] . "<br />";
-    echo $_COOKIE['age1'] . "<br />";
-    echo $_COOKIE['age2'] . "<br />";
-    echo $_COOKIE['age3'] . "<br />";
-}
